ebca845257e4223831408a4fe526f47e0f8d437a
MonikaMM77 <monika.mitka@gmail.com>
2018-10-21 12:59:48 +0200

    our initial commit

---
+++ hello.js
@@ -0,0 +1,18 @@
+
+var a = [4, 5, 7, 8]; //for defining an array , we use square brackets 
+a[3];//for accessing the elements of the array we need some sort of index and we use the name of the array then [] with the index
+var car = { "integer": 1, "float": 4.5, "string": "Hello", "array": a, 6: 8 };
+console.log(car.integer);
+console.log(car[6]);
+var paragraph = document.getElementById("paragraph");
+console.log(paragraph.innerText);
+paragraph.innerText = "hiiiii";
+
+        //document.
+
+        //this is a single line comment
+        /*
+        this is a multiple line comment
+        another line
+        
+        */
