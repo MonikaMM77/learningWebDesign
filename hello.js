a47302e06260664854eb852eba9bca9712dd5d54
MonikaMM77 <monika.mitka@gmail.com>
2018-11-18 14:00:15 +0100

    json object was parsed into html

--- hello.js
+++ hello.js
@@ -38,12 +38,39 @@
 
 
 function loadDoc() {
-        var city = document.getElementById("txt");
+        var city = document.getElementById("txt").value;
+
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
                 if (this.readyState == 4 && this.status == 200) {
+
+                        var weather = JSON.parse(this.responseText);
+                        console.log(x = weather)
+                        var tempInC = (weather.main.temp - 273.15);
+                        document.getElementById("cityName").innerText = weather.name;
+                        document.getElementById("countryCode").innerText = weather.sys.country;
+                        document.getElementById("coordinates").innerText = "longitude: " + weather.coord.lon + " latitude: " + weather.coord.lat;
+                        document.getElementById("weatherStatus").innerText = weather.weather[0].main;
+                        document.getElementById("Wind").innerText = " wind speed: " + weather.wind.speed + " degree of the wind: " + weather.wind.deg;
+                        document.getElementById("Clouds").innerText = weather.clouds.all
+                        document.getElementById("countryCode").innerText = weather.sys.country;
+                        document.getElementById("TemperatureinCelsius").innerText = tempInC;
+                        document.getElementById("TemperatureinFahrenheit").innerText = (weather.main.temp * 9 / 5 - 459.67);
                         
-                               var weather =  this.responseText;
+                        document.getElementById("message").innerText = tempInC >= 10? "happy warm weather":"sad cold weather";
+
+                        // if (tempInC >= 10) {
+                        //         document.getElementById("message").innerText = "happy warm weather";
+                        // } else {document.getElementById("message").innerText = "sad cold weather" ;}
+
+
+                        //document.getElementById("message").inerText = 
+
+
+
+
+
+
                 }
         };
         xhttp.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=e7a8bdd334c740cd19bde3747e110a49", true);
